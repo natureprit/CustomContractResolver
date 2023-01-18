3da5c7f20ca90b79d56be02aed59e627742dc51a
Pritesh Bhanage <pritesh.bhanage@syngenta.com>
2023-01-18 11:20:32 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            using (StreamReader r = new StreamReader(@"C:\\Users\\Pritesh_Bhanage\\source\\repos\\WebApplication1\\WebApplication1\\Controllers\\file.json"))
+            {
+                string json = r.ReadToEnd();
+                var settings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CustomContractResolver(),
+                };
+                Product item = JsonConvert.DeserializeObject<Product>(json, settings);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class CustomContractResolver : DefaultContractResolver
+    {
+        private Dictionary<string, string> PropertyMappings { get; set; }
+
+        public CustomContractResolver()
+        {
+            this.PropertyMappings = new Dictionary<string, string>
+        {
+            {"Id", "ProductId"},
+            {"Id","ProdId" },
+            {"Name", "ProductName"},
+            {"Description", "ProductDesc"},
+            {"Category", "ProductCategory"},
+        };
+        }
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            string resolvedName = null;
+            var resolved = this.PropertyMappings.TryGetValue(propertyName, out resolvedName);
+            return (resolved) ? resolvedName : base.ResolvePropertyName(propertyName);
+        }
+    }
+}
